5b6e8fe3c71b2d9f1d5514f5e03ebf52f9f1663b
ridhoalrafi1009 <rihdoalrafi@gmail.com>
2020-10-23 23:33:06 +0700

    Tugas3

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -31,6 +31,17 @@ $routes->setAutoRoute(true);
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
+$routes->get('/mahasiswa', function(){
+	$data['judul']='Mahasiswa';
+	$data['h1']= 'Table Mahasiswa';
+	$data['nama']='Ridho Alrafi';
+	$data['npm']='1817051009';
+	$data['semester']='5';
+	echo view('mahasiswa/header', $data);
+	echo view('mahasiswa/index', $data);
+	echo view('mahasiswa/footer');
+});
+
 
 /**
  * --------------------------------------------------------------------
--- app/Views/mahasiswa/index.php
+++ app/Views/mahasiswa/index.php
@@ -1,2 +1,13 @@
-<h1><?=$nama?></h1>
-<h2><?=$npm?></h2>
+<h1><?= $h1 ?></h>
+<table border="2">
+    <tr>
+        <th>NAMA</th>
+        <th>NPM</th>
+        <th>SEMESTER</th>
+    </tr>
+    <tr>
+        <td><?= $nama?></td>
+        <td><?= $npm?></td>
+        <td style="text-align:center;"><?= $semester?></td>
+    </tr>
+</table>
